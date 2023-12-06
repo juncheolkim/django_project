7d2e100104ce60fde8c397aaccedb288423e55a7
juncheolkim <kimjoon889@gmail.com>
2023-12-06 17:26:14 +0900

    ✨ detail 페이지 view 구현

--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 
 
@@ -6,3 +6,8 @@ def index(request):
     post_list = Post.objects.order_by("-create_date")
     context = {"post_list": post_list}
     return render(request, "board/post_list.html", context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
+    return render(request, 'board/post_detail.html', context)
