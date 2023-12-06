24c7958dcaa869c9fa9917febff9c58ba6c910d8
juncheolkim <kimjoon889@gmail.com>
2023-12-06 17:23:02 +0900

    ✨ URL 별칭과 네임스페이스 설정
    각 path의 name 속성값을 설정하여 별칭을 정할 수 있다. 이 별칭은 템플릿이나 redirect할 때 사용된다. 하지만, 여러개의 app이 있을 때는 이 별칭이 겹치면 안되기 때문에 곤란해질 수 있다.
    이 때 사용할 수 있는 것이 네임스페이스이다. 네임스페이스를 urls 파일에 app_name으로 지정하면, 별칭이 같아도 구분할 수 있게된다.

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "board"
+
 urlpatterns = [
-    path('', views.index),
+    path("", views.index, name='index'),
+    path("<int:post_id>/", views.detail, name='detail')
 ]
