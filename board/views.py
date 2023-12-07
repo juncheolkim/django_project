10af0a2395690951c165a9fed28f73cbeba4d088
juncheolkim <kimjoon889@gmail.com>
2023-12-07 15:45:07 +0900

    ✨ 페이징 구현
    게시판 페이지네이션 구현.
    장고에서는 Paginator 클래스를 사용하여 페이징을 구현할 수 있다.
    장고는 기본적으로 LazyLoading으로 쿼리를 요청한다고 한다. 즉, model객체를 식별자에 연결해서 정렬하는 시점이 아니라, 실제로 데이터를 사용할 때 필요한 만큼에 대해서만 쿼리를 보낸다.
    참고: https://leffept.tistory.com/311
    page_obj = paginator.get_page(page)
    paginator를 이용하여 요청된 페이지(page)에 해당되는 페이징 객체(page_obj)를 생성했다. 이렇게 하면 장고 내부적으로는 데이터 전체를 조회하지 않고 해당 페이지의 데이터만 조회하도록 쿼리가 변경된다.

--- board/views.py
+++ board/views.py
@@ -3,11 +3,15 @@ from django.utils import timezone
 from django.http import HttpResponseNotAllowed
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
+from django.core.paginator import Paginator  
 
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     post_list = Post.objects.order_by("-create_date")
-    context = {"post_list": post_list}
+    paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {"post_list": page_obj}
     return render(request, "board/post_list.html", context)
 
 def detail(request, post_id):
