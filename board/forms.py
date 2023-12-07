12fe2b9b1c9830e52e72e6fd07c12ce9cee53297
juncheolkim <kimjoon889@gmail.com>
2023-12-07 11:51:22 +0900

    ✨ 게시글 등록 form 생성
    forms.ModelForm을 상속받아서 form을 생성한다.
    forms.ModelForm은 내부 클래스 Meta를 필수적으로 설정한다.
    Meta 속성 중 model, fields 필수적으로 명시하며, labels는 해당 속성들을 어떻게 표현할 것인지 설정한다.

---
+++ board/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from board.models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post  # 사용할 모델
+        fields = ["subject", "content"]  # PostForm에서 사용할 Post 모델의 속성
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }  
