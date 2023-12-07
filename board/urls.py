7fee61fef405971c49dcb49384ddd86d021b4d19
juncheolkim <kimjoon889@gmail.com>
2023-12-07 11:51:02 +0900

    ✨ 게시글 등록 urls 설정

--- board/urls.py
+++ board/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "board"
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("<int:post_id>/", views.detail, name='detail'),
-    path('comment/create/<int:post_id>/', views.comment_create, name='comment_create'),
+    path("", views.index, name="index"),
+    path("<int:post_id>/", views.detail, name="detail"),
+    path("comment/create/<int:post_id>/", views.comment_create, name="comment_create"),
+    path("post/create/", views.post_create, name="post_create"),
 ]
