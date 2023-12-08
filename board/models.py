1dc06b08072e22ae696aa9d361bb10d753e1ac7d
juncheolkim <kimjoon889@gmail.com>
2023-12-08 12:30:27 +0900

    ✨ Post, Comment 모델 수정
    User 모델은 django.contrib.auth 앱이 제공하는 사용자 모델로 회원가입시 데이터 저장에 사용했던 모델이다.
    모델을 변경한 후에는 반드시 makemigrations와 migrate를 통해 데이터베이스를 변경해 주어야 한다.
    파이썬 셸에서 테스트를 위해 작성한 게시글 및 댓글의 값을 null로 세팅하면 안되기 때문에, 1로 세팅하도록 한다(1은 admin의 pk).

--- board/models.py
+++ board/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
 
 
 class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
