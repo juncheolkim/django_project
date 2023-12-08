33c4b60c7110b763e63a8a158bc0c036f7471b46
juncheolkim <kimjoon889@gmail.com>
2023-12-08 21:07:44 +0900

    ✨ 추천 urls 설정

--- board/urls.py
+++ board/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
         post_views.post_delete,
         name="post_delete",
     ),
+    path('post/vote/<int:post_id>/', post_views.post_vote, name='post_vote'),
     # comment_views.py
     path(
         "comment/create/<int:post_id>/",
@@ -36,4 +37,5 @@ urlpatterns = [
         comment_views.comment_delete,
         name="comment_delete",
     ),
+    path('comment/vote/<int:comment_id>/', comment_views.comment_vote, name='comment_vote'),
 ]
