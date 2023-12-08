1dc0d2b24d8f47e10f508040c47890bd4bcf7d0d
juncheolkim <kimjoon889@gmail.com>
2023-12-08 20:38:54 +0900

    ✨ 추천인 속성 설정
    Post, Comment 모델에 추천인 속성을 설정한다. ManyToManyField로써 유저가 역참조를 할 수 있는데, 이 때 author역참조와 호출이 겹치게 되기 때문에 related_name으로 구분해준다.

--- board/models.py
+++ board/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_post')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-
+    voter = models.ManyToManyField(User, related_name='voter_post')  # 추천인 추가
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comment')
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_comment')
