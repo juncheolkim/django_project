8d61076b474b257b4dd117b9bc75d9d7252582fa
juncheolkim <kimjoon889@gmail.com>
2023-12-08 12:35:46 +0900

    ✨ views 작성자 권한 설정
    게시글 혹은 댓글을 달 때마다 author 속성에 값을 넣어줘야 한다.
    request.user에는 로그아웃 상태이면 AnonymousUser 객체가, 로그인 상태이면 User 객체가 들어있다.
    @login_required 애너테이션이 붙은 함수는 로그인이 필요한 함수를 의미한다. 이를 사용하여 로그인한 유저만 요청을 수행하도록 하며, 권한이 없을 경우 로그인 페이지로 이동시킨다.
    댓글을 달 때 로그인을 수행하면 405 오류가 발생한다. 왜냐면 comment_create 함수를 GET으로 요청하면 오류를 발생시키도록 코딩했기 때문이다.
    따라서 해당 상세페이지로 redirect 하도록 수정했다.

--- board/views.py
+++ board/views.py
@@ -1,48 +1,57 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.http import HttpResponseNotAllowed
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
-from django.core.paginator import Paginator  
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    page = request.GET.get('page', '1')  # 페이지
+    page = request.GET.get("page", "1")  # 페이지
     post_list = Post.objects.order_by("-create_date")
     paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
     context = {"post_list": page_obj}
     return render(request, "board/post_list.html", context)
 
+
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    context = {'post': post}
-    return render(request, 'board/post_detail.html', context)
+    context = {"post": post}
+    return render(request, "board/post_detail.html", context)
 
+
+# request.user에는 로그아웃 상태이면 AnonymousUser 객체가, 로그인 상태이면 User 객체가 들어있다.
+# @login_required 애너테이션이 붙은 함수는 로그인이 필요한 함수를 의미한다.
+@login_required(login_url="common:login")
 def comment_create(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
+            comment.author = request.user  # author 속성에 로그인 계정 저장
             comment.create_date = timezone.now()
             comment.post = post
             comment.save()
-            return redirect('board:detail', post_id=post.id)
+            return redirect("board:detail", post_id=post.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
-    context = {'post': post, 'form': form}
-    return render(request, 'board/post_detail.html', context)
+        return redirect("board:detail", post_id=post.id)
+    context = {"post": post, "form": form}
+    return render(request, "board/post_detail.html", context)
 
+
+@login_required(login_url="common:login")
 def post_create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            post.author = request.user  # author 속성에 로그인 계정 저장
             post.create_date = timezone.now()
             post.save()
-            return redirect('board:index')
+            return redirect("board:index")
     else:
         form = PostForm()
-    context = {'form': form}
-    return render(request, 'board/post_form.html', context)
+    context = {"form": form}
+    return render(request, "board/post_form.html", context)
