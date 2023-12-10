bacb132915dd6a7dabedaf744a2fefc9e4f3464f
juncheolkim <kimjoon889@gmail.com>
2023-12-11 02:29:42 +0900

    🔧 도메인 설정 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ["35.216.16.171"]
+ALLOWED_HOSTS = ["35.216.16.171", "lit-board.site"]
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
