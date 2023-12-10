a0316262a0d327096e312ca4c4e8ce4631a0c7d4
juncheolkim <kimjoon889@gmail.com>
2023-12-10 15:33:03 +0900

    ✨ 검색 함수 구현
    GET이 아닌 POST 방식으로 검색과 페이징을 처리한다면 웹 브라우저에서 "새로고침" 또는 "뒤로가기"를 했을 때 "만료된 페이지입니다."라는 오류를  만나게 된다. 왜냐하면 POST 방식은 동일한 POST 요청이 발생할 경우 중복 요청을 방지하기 위해 "만료된 페이지입니다." 라는 오류를 발생시키기 때문이다. 2페이지에서 3페이지로 갔다가 뒤로가기를 했을 때 2페이지로 가는것이 아니라 오류가 발생한다면 엉망이 될 것이다.
    이러한 이유로 여러 파라미터를 조합하여 GET 방식을 사용하여 구현한다.
    Q함수내에 사용된 subject__icontains=kw의 의미는 제목에 kw 문자열이 포함되었는지를 의미한다. answer__author__username__icontains 은 좀 복잡해 보이는데 "답변을 작성한 사람의 이름에 포함되는가?" 라는 의미를 갖는다. filter 함수에서 모델 속성에 접근하기 위해서는 이처럼 __ (언더바 두개) 를 이용하여 하위 속성에 접근할 수 있다.
    그리고 page와 kw를 템플릿에 전달하기 위해 context 딕셔너리에 추가한다.

--- board/views/base_views.py
+++ board/views/base_views.py
@@ -1,14 +1,27 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from ..models import Post
+from django.db.models import Q
 
 
 def index(request):
     page = request.GET.get("page", "1")  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
     post_list = Post.objects.order_by("-create_date")
+
+    if kw:
+        # subject__contains=kw 대신 subject__icontains=kw을 사용하면 대소문자를 가리지 않고 찾아 준다.
+        post_list = post_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(comment__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(comment__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+
     paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {"post_list": page_obj}
+    context = {"post_list": page_obj, 'page': page, 'kw': kw}
     return render(request, "board/post_list.html", context)
 
 
